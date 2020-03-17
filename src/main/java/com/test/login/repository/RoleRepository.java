f752bb49fe224ab15ed040743f7fcd4f78157c73
Fercho <ultrafenixjf@gmail.com>
2020-03-17 11:17:42 -0600

    test

---
+++ src/main/java/com/test/login/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.test.login.repository;
+
+import com.test.login.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Role findByRole(String role);
+
+}
